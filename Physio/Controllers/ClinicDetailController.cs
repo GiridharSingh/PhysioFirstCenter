e7924bdf9a95b6d946a9a12e4f7172a5197024b9
giridhars <giridhars@chetu.com>
2017-05-26 18:07:36 +0530

    Implemented new page for "About Clinic detail"

---
+++ Physio/Controllers/ClinicDetailController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+using System.Net;
+
+namespace Physio.Controllers
+{
+    [HandleError]
+    public class ClinicDetailController : Controller
+    {
+        // GET: ClinicDetail
+        private AboutClinicDAL objAboutClinic;
+        public ActionResult Index(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            objAboutClinic = new AboutClinicDAL();
+            var AboutClinic = objAboutClinic.GetAbout(id);
+            if (AboutClinic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(AboutClinic);
+        }
+    }
+}
